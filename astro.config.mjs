e4961a50486e54969673b603ca22dbce6848ee95
cc <chao921125@sina.com>
2023-03-20 16:29:52 +0800

    gengxin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), react(), image(), sitemap(), tailwind()]
+  integrations: [vue({
+    template: {
+      compilerOptions: {
+        // treat any tag that starts with ion- as custom elements
+        isCustomElement: tag => tag.startsWith('ion-')
+      }
+    }
+    // ...
+  }), react(), image(), sitemap(), tailwind()]
 });
